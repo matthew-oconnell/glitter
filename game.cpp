88581f79d266bf41ee7804e4a3a9e7419bf340ef
Matthew O'Connell <matthew.david.oconnell@gmail.com>
2018-07-15 18:34:41 -0400

    To set up GLEW

--- game.cpp
+++ game.cpp
@@ -1,7 +1,7 @@
-
 #include <window.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 void initialize();
 void finalize();
@@ -13,27 +13,33 @@ int main() {
   Glitter::Graphics::Window game_window("Glitter", 800, 600);
   glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
-  loop(game_window);
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
 
+  loop(game_window);
   finalize();
+  return 0;
 }
 
 void loop(Glitter::Graphics::Window& game_window) {
   while(!game_window.closed()){
     game_window.clear();
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f( 0.5f,  0.5f);
-    glVertex2f( 0.5f, -0.5f);
-    glEnd();
+//    glBegin(GL_TRIANGLES);
+//    glVertex2f(-0.5f, -0.5f);
+//    glVertex2f( 0.5f,  0.5f);
+//    glVertex2f( 0.5f, -0.5f);
+//    glEnd();
+    glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
     game_window.update();
   }
 }
 
 void initialize() {
+  std::cout << "Initializing Glitter" << std::endl;
   if( !glfwInit())
     throw std::logic_error("Could not initialize GLFW!");
-  std::cout << glGetString(GL_VERSION);
+  std::cout << "glfw initialized" << std::endl;
 }
 void finalize() {
   glfwTerminate();
