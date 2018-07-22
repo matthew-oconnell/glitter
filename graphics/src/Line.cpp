75196f759c1ba28517491613c0b38f585c5a8849
Matthew O'Connell <matthew.david.oconnell@gmail.com>
2018-07-21 22:07:47 -0400

    To render a not so cheating aim line.

--- graphics/src/Line.cpp
+++ graphics/src/Line.cpp
@@ -20,8 +20,12 @@ void Line::render(Math::Vec2d world_location, Screen *s) {
   auto world_begin = begin + world_location;
   auto world_end = end + world_location;
   glBegin(GL_LINE_STRIP);
+  auto direction = (world_end - world_begin);
+  direction *= 1.0f/std::sqrt(direction.x*direction.x + direction.y*direction.y);
+  direction *= 1.1f;
   auto render_begin = s->convertWorldToRender(world_begin);
-  auto render_end = s->convertWorldToRender(world_end);
+  auto render_end = s->convertWorldToRender(world_begin + direction);
+  std::cout << render_begin << " " << render_end << std::endl;
   glColor4f(1.0f,0.0f,0.0f,1.0f);
   glVertex2d(render_begin.x, render_begin.y);
   glVertex2d(render_end.x, render_end.y);
